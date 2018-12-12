8ae73c6570770b9779c2abedb5ccdbeff2f81f1d
cpfyjjs <cpfyjjs@foxmail.com>
2018-12-12 20:33:47 +0800

    12

---
+++ project01/project01/spiders/chouti.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class ChoutiSpider(scrapy.Spider):
+    name = 'chouti'
+    allowed_domains = ['chouti.com']
+    start_urls = ['http://chouti.com/']
+
+    def parse(self, response):
+        from scrapy.http.response.html import HtmlResponse
+
+        print(response.text)
+        response.xpath('//')
+
+
+
+
